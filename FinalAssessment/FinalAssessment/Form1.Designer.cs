89deca26644274c1c8a5603bec66d8a7988d3bf5
Whitney Haddow <whitneyhaddow@gmail.com>
2014-10-20 14:21:57 -0600

    Initial Commit

---
+++ FinalAssessment/FinalAssessment/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace FinalAssessment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.lstSalaries = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.lblAvg = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.lblMidpoint = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Annual Salary:";
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(207, 75);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(141, 23);
+            this.txtSalary.TabIndex = 1;
+            // 
+            // lstSalaries
+            // 
+            this.lstSalaries.FormattingEnabled = true;
+            this.lstSalaries.ItemHeight = 16;
+            this.lstSalaries.Location = new System.Drawing.Point(419, 75);
+            this.lstSalaries.Name = "lstSalaries";
+            this.lstSalaries.Size = new System.Drawing.Size(118, 324);
+            this.lstSalaries.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(416, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Salaries:";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(137, 116);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(103, 32);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "&Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnDone
+            // 
+            this.btnDone.Location = new System.Drawing.Point(246, 116);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(103, 32);
+            this.btnDone.TabIndex = 5;
+            this.btnDone.Text = "&Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // lblAvg
+            // 
+            this.lblAvg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAvg.Location = new System.Drawing.Point(66, 163);
+            this.lblAvg.Name = "lblAvg";
+            this.lblAvg.Size = new System.Drawing.Size(283, 27);
+            this.lblAvg.TabIndex = 6;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(236, 367);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(103, 32);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Calculated Federal Tax:";
+            // 
+            // lblTax
+            // 
+            this.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTax.Location = new System.Drawing.Point(208, 227);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(141, 27);
+            this.lblTax.TabIndex = 9;
+            // 
+            // lblMidpoint
+            // 
+            this.lblMidpoint.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMidpoint.Location = new System.Drawing.Point(208, 267);
+            this.lblMidpoint.Name = "lblMidpoint";
+            this.lblMidpoint.Size = new System.Drawing.Size(141, 27);
+            this.lblMidpoint.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(53, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Midpoint Salary:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnNext;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(585, 430);
+            this.Controls.Add(this.lblMidpoint);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblTax);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblAvg);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstSalaries);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Calculating Taxes Based on Salary";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.ListBox lstSalaries;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.Label lblAvg;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTax;
+        private System.Windows.Forms.Label lblMidpoint;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ FinalAssessment/FinalAssessment/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalAssessment
+{
+    public partial class Form1 : Form
+    {
+        //declare an array
+        const int MAX = 50;
+        double[] salaryContainer = new double[MAX];
+        int count = 0; //default number of values is 0
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            //validate data
+            if (IsPositiveDouble(txtSalary, "Salary"))
+            {
+                if (count < MAX)
+                {
+                    salaryContainer[count] = Double.Parse(txtSalary.Text);
+                    count++;
+                    txtSalary.Text = "";
+                    txtSalary.Focus();
+                }
+                else
+                {
+                    btnDone.Focus();
+                    btnNext.Enabled = false;
+                    MessageBox.Show("You cannot enter any more salary values.",
+                           "Input Error");
+                }
+
+                //display salary in list box
+
+                lstSalaries.Items.Clear();
+                for (int i = 0; i < count; i++)
+                {
+                    lstSalaries.Items.Add(salaryContainer[i].ToString("c"));
+                }
+
+                //display federal tax
+                double federalTax = CalculateTax(salaryContainer, count);
+                lblTax.Text = federalTax.ToString("c");
+
+
+            }
+
+
+        }
+
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+            //make sure there is at least one salary entered
+            if (count < 1)
+            {
+                MessageBox.Show("Please enter at least 1 salary.");
+            }
+            else
+            {
+                //get the sum
+                double salarySum = Sum(salaryContainer, count);
+
+                //get the average and display
+                double average = salarySum / count;
+                lblAvg.Text = "The average salary is " + average.ToString("c");
+
+                //find the  smallest
+                double small = Min(salaryContainer, count);
+
+                //find the largest
+                double large = Max(salaryContainer, count);
+
+                //find and display the midpoint
+                double midpoint = (small + large) / 2;
+                lblMidpoint.Text = midpoint.ToString("c");
+            }
+        }
+
+
+        //method to calculate sum of values in an array
+        public double Sum(double[] numbers, int howMany)
+        {
+            double result = 0;
+            for (int i = 0; i < howMany; i++)
+            {
+                result = result + numbers[i];
+            }
+            return result;
+        }
+
+        //method to find smallest salary value
+        public double Min(double[] numbers, int howMany)
+        {
+            double smallest = numbers[0]; //suppose the first element is the smallest
+            for (int i = 0; i < howMany; i++)
+            {
+                if (numbers[i] < smallest) //found a smaller one
+                {
+                    smallest = numbers[i];
+                }
+            }
+            return smallest;
+        }
+
+        //method to find largest salary value
+        public double Max(double[] numbers, int howMany)
+        {
+            double biggest = numbers[0]; //suppose the first element is the smallest
+            for (int i = 0; i < howMany; i++)
+            {
+                if (numbers[i] > biggest) //found a smaller one
+                {
+                    biggest = numbers[i];
+                }
+            }
+            return biggest;
+        }
+
+        //function to validate if a text box is above zero and a double
+        public static bool IsPositiveDouble(TextBox inputBox, string inputName)
+        {
+            bool result = true; //default
+            double temp; //temporary variable for try parse
+            if (!Double.TryParse(inputBox.Text, out temp)) //bad number format
+            {
+                MessageBox.Show(inputName + " has to be a numeric value.",
+                    "Input Error");
+                result = false;
+            }
+            else if (temp <= 0) //zero or negative
+            {
+                MessageBox.Show(inputName + " must be higher than 0.",
+                    "Input Error");
+                result = false;
+            }
+            return result;
+        }
+
+
+        //method to calculate federal tax                  
+        public double CalculateTax(double[] salaryContainer, int howMany)
+        {
+            double tax = 0;
+            for (int i = 0; i < howMany; i++)
+            {
+                if (salaryContainer[i] <= 43561)
+                    tax = salaryContainer[i] * 0.15;
+                else if (salaryContainer[i] > 43561 && salaryContainer[i] <= 87123)
+                    tax = ((salaryContainer[i] - 43561) * 0.22) + 6534;
+                else if (salaryContainer[i] > 87123)
+                    tax = ((salaryContainer[i] - 87123) * 0.26) + 16118;
+            }
+            return tax;
+
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close(); //terminate application
+        }
+
+    }
+
+}
+
+
+       
